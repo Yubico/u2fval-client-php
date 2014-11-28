f3848c7da7e3175e81f4d870863623570d7cda36
Dain Nilsson <dain@yubico.com>
2014-11-28 11:55:33 +0100

    Added U2F devices page.

---
+++ examples/tutorial/devices.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This page shows the user his/her registered U2F devices.
+ *
+ */
+
+require_once('users.inc.php');
+require_once('template.inc.php');
+
+require_once('../config.php');
+
+//Ensure that the user is logged in.
+$user = get_user();
+if(empty($user)) {
+  header('Location: index.php');
+  die();
+}
+
+print_header($user);
+
+?>
+      <div class="row">
+        <div class="col-md-4">
+          <h2>U2F devices</h2>
+<?php
+
+//Get a list of the users U2F devices from the U2FVAL service.
+$devices = $u2fval->list_devices(get_id($user));
+if(empty($devices)) {
+  echo "<p>You have no devices registered.</p>";
+} else {
+  foreach($devices as $device) {
+    //Prints metadata about each device, and when it was registered and last used.
+    $metadata = $device['metadata'];
+    echo "<div class=\"well well-sm\">";
+    echo "<h3>".$metadata['device']['displayName']."</h3>";
+    if(!empty($metadata['device']['imageUrl'])) {
+      echo "<img src=\"".$metadata['device']['imageUrl']."\" width=\"80\" height=\"80\" /><br/>";
+    }
+    echo "<strong>Registered: ".date("H:i D M j", strtotime($device['created']))."</strong><br/>";
+    echo "<strong>Last used: ".date("H:i D M j", strtotime($device['lastUsed']))."</strong><br/>";
+    echo "</div>";
+  }
+}
+
+?>
+          <p><a href="welcome.php">Back</a>.</p>
+        </div>
+      </div>
+<?php
+
+print_footer();
+
+?>
--- examples/tutorial/welcome.php
+++ examples/tutorial/welcome.php
@@ -16,6 +16,7 @@ print_header($user);
         <div class="col-md-4">
           <h2>Logged in</h2>
           <p>Congratulations! You are logged in as <?php echo ($user); ?>.</p>
+          <p><a href="devices.php">Manage U2F devices</a></p>
           <p><a href="index.php">Log out.</a></p>
         </div>
       </div>
