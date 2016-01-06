1d16da5951125ec7ab80e7d1ca3eb7e7c7ee8e5f
JamesCullum <nicolas.giese@web.de>
2016-01-06 22:02:42 +0100

    Cleaning, instructions for non-composer users, better example
    - Indentation improved, it was pretty hard to read
    - Composer appears to be an overkill for me for only having CURL as
    dependancy which is pretty common already. Therefore added instructions
    on how to avoid it.
    - The example was neither easy to read, nor explained in any way. This
    minified version shows the structure and workflow better.

--- examples/config.php
+++ examples/config.php
@@ -4,18 +4,22 @@
  * Configuration file for all examples.
  */
 
+// Using Composer
 require_once(dirname(__FILE__).'/../vendor/autoload.php');
+/* or by hand
+require_once(dirname(__FILE__)."/../src/Client.php");
+require_once(dirname(__FILE__)."/../src/Exceptions.php");
+if(!function_exists("curl_init")) die("You require CURL for PHP");
+*/
 
 // U2FVAL Configuration - Using API token authentication:
 $u2fval = U2fVal\Client::withApiToken(
-  'https://u2fval.appspot.com/api',               // URL of the U2FVAL service.
-  ''  // API token for use with service, fill this in!
+	'https://u2fval.appspot.com/api',	// URL of the U2FVAL service
+	''  								// API token for use with service, get it from u2fval.appspot.com and fill in here
 );
 
-// Alternate configurations available: See Client.php for more details.
-
+// Alternate configurations available: See Client.php for more details
 //$u2fval = U2fVal\Client::withNoAuth();
-
 //$u2fval = U2fVal\Client::withHttpAuth('username', 'password');
 
 ?>
