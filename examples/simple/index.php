efaae757803c21445ca2707d27533da29fbcf2bc
JamesCullum <nicolas.giese@web.de>
2016-01-09 14:47:42 +0100

    Better example & indentation
    - Indentation improved, it was pretty hard to read. Now used spaces
    instead of tabs, even though i consider it very bad practice.
    - The example was neither easy to read, nor explained in any way. This
    minified version shows the structure and workflow better.

--- examples/simple/index.php
+++ examples/simple/index.php
@@ -1,136 +1,61 @@
 <?php
+require_once(dirname(__FILE__)."/../config.php");
 
-/* Copyright (c) 2014 Yubico AB
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-require_once('../config.php');
-
-?>
-
-<html>
-<head>
-<title>PHP U2F example</title>
-
-<script src="u2f-api.js"></script>
-
-<script>
-<?php
-
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-  if(!$_POST['username']) {
-    echo "alert('no username provided!');";
-  } else if(!isset($_POST['action']) && !isset($_POST['register2']) && !isset($_POST['authenticate2'])) {
-    echo "alert('no action provided!');";
-  } else {
-    $user = $_POST['username'];
-    echo "var username = '" . $user . "';";
-
-    if(isset($_POST['action'])) {
-      if($_POST['action'] === 'register') {
-        try {
-          $data = $u2fval->register_begin($user);
-          echo "var data = " . $data . ";";
-?>
-          setTimeout(function() {
-            console.log("Data: ", data);
-            console.log("Register Requests: ", data.registerRequests);
-            console.log("Authenticate Requests: ", data.authenticateRequests);
-            u2f.register(data.registerRequests, data.authenticateRequests, function(resp) {
-              var form = document.getElementById('form');
-              var reg = document.getElementById('register2');
-              var user = document.getElementById('username');
-              console.log("Register callback", resp);
-              if(resp.errorCode) {
-                  alert("registration failed with error: " + resp.errorCode);
-                  return;
-              }
-              reg.value = JSON.stringify(resp);
-              user.value = username;
-              form.submit();
-            });
-          }, 1000);
-<?php
-        } catch(U2fVal\U2fValException $exception) {
-          echo "alert('error: " . $exception->getMessage() . "');";
-        }
-      } else if($_POST['action'] === 'authenticate') {
-        try {
-          $data = $u2fval->auth_begin($user);
-          echo "var data = " . $data . ";";
-?>
-          setTimeout(function() {
-            console.log("Data: ", data);
-            console.log("Authenticate Requests: ", data.authenticateRequests);
-            u2f.sign(data.authenticateRequests, function(resp) {
-              var form = document.getElementById('form');
-              var auth = document.getElementById('authenticate2');
-              var user = document.getElementById('username');
-              console.log("Authenticate callback", resp);
-              auth.value = JSON.stringify(resp);
-              user.value = username;
-              form.submit();
-            });
-          }, 1000);
-<?php
-        } catch(U2fVal\U2fValException $exception) {
-          echo "alert('error: " . $exception->getMessage() . "');";
-        }
-      }
-    } else if($_POST['register2']) {
-      try {
-        $data = $u2fval->register_complete($user, $_POST['register2']);
-        echo "alert('success: " . json_encode($data) . "');";
-      } catch(U2fVal\U2fValException $exception) {
-        echo "alert('error: " . $exception->getMessage() . "');";
-      }
-    } else if($_POST['authenticate2']) {
-      try {
-        $data = $u2fval->auth_complete($user, $_POST['authenticate2']);
-        echo "alert('success: " . json_encode($data) . "');";
-      } catch(U2fVal\U2fValException $exception) {
-        echo "alert('error: " . $exception->getMessage() . "');";
-      }
-    }
-  }
+if(empty($_GET["action"])) // Let the User choose an option and ID for testing purposes
+{
+	echo '<form action="index.php" method="get">
+		<input type="text" name="username" placeholder="Username"><br>
+		<select name="action">
+			<option value="register">Register</option>
+			<option value="login">Login</option>
+		</select><br>
+		<input type="submit">
+	</form>';
+	exit;
+}
+else // Register or Login
+{
+	$user = $_GET["username"];
+	$isregister = ($_GET["action"] == "register");
+	if(empty($_GET["data"])) // Ask user to press yubikey to register or login
+	{
+		$data = $isregister ? $u2fval->register_begin($user) : $u2fval->auth_begin($user);
+		echo '
+		<script src="u2f-api.js"></script>
+		<script>
+			var data = '.$data.';
+			setTimeout(function() {
+				console.log("init",data);
+				';
+				
+		if($isregister) echo 'u2f.register(data.registerRequests, data.authenticateRequests, function(resp) {';
+		else echo 'u2f.sign(data.authenticateRequests, function(resp) {';
+		
+		echo '
+					console.log("callback",resp);
+					if(resp.errorCode) {
+						alert("registration failed with error: " + resp.errorCode);
+						return;
+					}
+					window.location = window.location.href + "&data=" + encodeURIComponent(JSON.stringify(resp));
+				});
+				alert("Touch Yubikey now (and create a gui for this app)");
+			  }, 1000);
+		</script>
+		Please wait...
+		<br><br><a href="index.php">Back to Main</a>';
+		exit;
+	}
+	else // Process yubikey data
+	{
+		try {
+			$data = $isregister ? $u2fval->register_complete($user, $_GET['data']) : $u2fval->auth_complete($user, $_GET["data"]);
+			echo '<h1>Success</h1>'.json_encode($data);
+		} catch(U2fVal\U2fValException $exception) {
+			echo '<h1>Error</h1>'.$exception->getMessage();
+		}
+		echo '<br><br><a href="index.php">Back to Main</a>';
+		exit;
+	}
 }
 ?>
-</script>
-</head>
-<body>
-
-<form method="POST" id="form">
-username: <input name="username" id="username" value="<?php echo $_POST['username']; ?>" /><br/>
-register: <input value="register" name="action" type="radio"/><br/>
-authenticate: <input value="authenticate" name="action" type="radio"/><br/>
-<input type="hidden" name="register2" id="register2"/>
-<input type="hidden" name="authenticate2" id="authenticate2"/>
-<button type="submit">Submit!</button>
-</form>
-
-</body>
-</html>
